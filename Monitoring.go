2e6741e5f33542538fd3fc46826805a9f9b6a97b
global <asgagagsad@gmail.com>
2022-07-22 12:59:52 +0300

    fix memory leaking

--- Monitoring.go
+++ Monitoring.go
@@ -1,79 +1,121 @@
 package main
 
 import (
-	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/bwmarrin/discordgo"
 	"log"
 	"net/http"
+	_ "net/http/pprof"
+	"runtime/debug"
 	"strings"
+	"sync"
 	"time"
 )
 
-type config struct {
-	Token string
-	Url   string
-	ch    chan string
-}
-
 func main() {
-	ch1 := make(chan string)
-	ch2 := make(chan string)
-	DarkRustDm := config{
-		Token: "OTg0NTU0NjUzNDEwODE2MDYw.GBKmCj.GXfvWERGC8QB4NUvY1vnzSb3cE_Nr4mDbcJ6Vg",
-		Url:   "https://www.gs4u.net/ru/s/228660.html",
-		ch:    ch1,
-	}
-	DarkRustMain := config{
-		Token: "OTg0NTU0NzYzMzUxODkyMDIy.GkCJyf.lvozNzD7vpQkF2b-oJuqz0rcrjsdvUFb1nlEpk",
-		Url:   "https://www.gs4u.net/ru/s/229032.html",
-		ch:    ch2,
-	}
-	go DarkRustMain.getOnline()
-	go DarkRustMain.discordStatus()
-	go DarkRustDm.getOnline()
-	go DarkRustDm.discordStatus()
+	var wg sync.WaitGroup
 
-	time.Sleep(6256 * time.Hour)
+	wg.Add(2)
+	go func() {
+		gd, err := discordgo.New("OTg0NTU0NzYzMzUxODkyMDIy.Gt4xFm.qEdhxTkGc5-9MWr_isvaSk4FFYAEpFjLWZEM_Y")
+
+		if err != nil {
+			log.Println(err)
+		}
+		err = gd.Open()
+		if err != nil {
+			log.Println(err)
+		}
+		log.Println("bot dark main is up")
+		for {
+			data := darkMain()
+			err = gd.UpdateGameStatus(0, data)
+
+			if err != nil {
+				log.Println(err)
+			}
+			time.Sleep(10 * time.Second)
+			debug.FreeOSMemory()
+		}
+	}()
+
+	go func() {
+		gd, err := discordgo.New("OTg0NTU0NjUzNDEwODE2MDYw.GBVsca.2apCqCe6Mf2o7pbQXmFR7khypT_zdwAjsIhGJ0")
+		if err != nil {
+			log.Println(err)
+		}
+
+		err = gd.Open()
+		if err != nil {
+			log.Println(err)
+		}
+
+		log.Println("bot dark dm is up")
+
+		for {
+			data := darkDM()
+			err = gd.UpdateGameStatus(0, data)
+
+			if err != nil {
+				log.Println(err)
+			}
+			time.Sleep(10 * time.Second)
+			debug.FreeOSMemory()
+		}
+
+	}()
+	wg.Wait()
 
 }
 
-func (cfg *config) getOnline() {
-	for {
-		time.Sleep(time.Second * 5)
-		response, err := http.Get(cfg.Url)
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer response.Body.Close()
-		if response.StatusCode > 400 {
-			fmt.Println("Error:", response.StatusCode)
-		}
-		doc, err := goquery.NewDocumentFromReader(response.Body)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		a := doc.Find("div.text").Text()
-		a = strings.ReplaceAll(a, "\n", "")
-		a = strings.ReplaceAll(a, "\t", "")
-		cfg.ch <- a
+func darkMain() string {
 
-		continue
+	response, err := http.Get("https://www.gs4u.net/ru/s/229032.html")
+
+	if err != nil {
+		log.Println(err)
 	}
+	defer response.Body.Close()
+	if response.StatusCode > 400 {
+		log.Println(err)
+	}
+	doc, err := goquery.NewDocumentFromReader(response.Body)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	a := doc.Find("div.text").Text()
+	a = strings.ReplaceAll(a, "\n", "")
+	a = strings.ReplaceAll(a, "\t", "")
+	a = strings.ReplaceAll(a, " ", "")
+	a = strings.ReplaceAll(a, "из", "/")
+
+	return a
+
 }
-func (cfg *config) discordStatus() {
-	gd, err := discordgo.New(cfg.Token)
+func darkDM() string {
+
+	response, err := http.Get("https://www.gs4u.net/ru/s/228660.html")
+
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
-	err = gd.Open()
+	defer response.Body.Close()
+	if response.StatusCode > 400 {
+		log.Println(err)
+	}
+	doc, err := goquery.NewDocumentFromReader(response.Body)
+
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
-	fmt.Println("Bot is up")
-	fmt.Println("Use <ctrl+c> for exit")
-	for {
-		gd.UpdateGameStatus(0, <-cfg.ch)
-		time.Sleep(time.Second * 10)
-	}
+
+	a := doc.Find("div.text").Text()
+	a = strings.ReplaceAll(a, "\n", "")
+	a = strings.ReplaceAll(a, "\t", "")
+	a = strings.ReplaceAll(a, " ", "")
+	a = strings.ReplaceAll(a, "из", "/")
+
+	return a
 }
