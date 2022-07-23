021132a7f7e0ac68aca1f2f705dda9c78546d94b
global <asgagagsad@gmail.com>
2022-07-23 13:40:33 +0300

    hunt rust

--- Monitoring.go
+++ Monitoring.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/bwmarrin/discordgo"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
 	"runtime/debug"
-	"strings"
 	"sync"
 	"time"
 )
@@ -15,32 +15,10 @@ import (
 func main() {
 	var wg sync.WaitGroup
 
-	wg.Add(2)
-	go func() {
-		gd, err := discordgo.New("OTg0NTU0NzYzMzUxODkyMDIy.Gt4xFm.qEdhxTkGc5-9MWr_isvaSk4FFYAEpFjLWZEM_Y")
-
-		if err != nil {
-			log.Println(err)
-		}
-		err = gd.Open()
-		if err != nil {
-			log.Println(err)
-		}
-		log.Println("bot dark main is up")
-		for {
-			data := darkMain()
-			err = gd.UpdateGameStatus(0, data)
-
-			if err != nil {
-				log.Println(err)
-			}
-			time.Sleep(10 * time.Second)
-			debug.FreeOSMemory()
-		}
-	}()
+	wg.Add(1)
 
 	go func() {
-		gd, err := discordgo.New("OTg0NTU0NjUzNDEwODE2MDYw.GBVsca.2apCqCe6Mf2o7pbQXmFR7khypT_zdwAjsIhGJ0")
+		gd, err := discordgo.New("MTAwMDM0OTMwNjg2MTEyNTY3NA.GXJhSS.CEBhJhQtVuyeAYiTsxNwi850sEwoWhOKaQySgY")
 		if err != nil {
 			log.Println(err)
 		}
@@ -50,10 +28,11 @@ func main() {
 			log.Println(err)
 		}
 
-		log.Println("bot dark dm is up")
+		log.Println("bot hunt rust is up")
 
 		for {
-			data := darkDM()
+			data := huntMain()
+			fmt.Printf(data)
 			err = gd.UpdateGameStatus(0, data)
 
 			if err != nil {
@@ -68,35 +47,9 @@ func main() {
 
 }
 
-func darkMain() string {
+func huntMain() string {
 
-	response, err := http.Get("https://www.gs4u.net/ru/s/229032.html")
-
-	if err != nil {
-		log.Println(err)
-	}
-	defer response.Body.Close()
-	if response.StatusCode > 400 {
-		log.Println(err)
-	}
-	doc, err := goquery.NewDocumentFromReader(response.Body)
-
-	if err != nil {
-		log.Println(err)
-	}
-
-	a := doc.Find("div.text").Text()
-	a = strings.ReplaceAll(a, "\n", "")
-	a = strings.ReplaceAll(a, "\t", "")
-	a = strings.ReplaceAll(a, " ", "")
-	a = strings.ReplaceAll(a, "из", "/")
-
-	return a
-
-}
-func darkDM() string {
-
-	response, err := http.Get("https://www.gs4u.net/ru/s/228660.html")
+	response, err := http.Get("https://huntrust.shop/#/app/store/")
 
 	if err != nil {
 		log.Println(err)
@@ -111,11 +64,8 @@ func darkDM() string {
 		log.Println(err)
 	}
 
-	a := doc.Find("div.text").Text()
-	a = strings.ReplaceAll(a, "\n", "")
-	a = strings.ReplaceAll(a, "\t", "")
-	a = strings.ReplaceAll(a, " ", "")
-	a = strings.ReplaceAll(a, "из", "/")
+	a := doc.Find("div.").Text()
 
 	return a
+
 }
